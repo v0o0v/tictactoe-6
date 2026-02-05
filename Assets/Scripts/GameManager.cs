a26e825741b75082b9e2a173e12e6750eda0a6ce
김민석 <v0o0v@daum.net>
2026-02-05 16:19:30 +0900

    refactor GameManager to inherit from Singleton and add MainPanelController for UI interactions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using UnityEngine.SceneManagement;
 
 namespace Tictactoe {
 
-    public class GameManager : MonoBehaviour {
+    public class GameManager : Singleton<GameManager> {
 
-        
+        protected override void OnSceneLoad(Scene scene, LoadSceneMode mode){
+            
+        }
 
     }
 
---
+++ Assets/Scripts/MainPanelController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Tictactoe {
+
+    public class MainPanelController : MonoBehaviour {
+
+        public void onClickSinglePlayButton(){
+            
+        }
+
+        public void onClickDualPlayButton(){ }
+
+        public void onClickSettingsButton(){ }
+
+    }
+
+}
