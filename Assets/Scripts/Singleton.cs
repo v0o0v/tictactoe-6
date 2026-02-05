8b34b01794fe2d7b760971ce0af35dca74a9c44c
김민석 <v0o0v@daum.net>
2026-02-05 15:11:09 +0900

    add Singleton base class for Unity components

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Component {
+
+    private static T _instance;
+
+    public static T Instance{
+        get{
+            if (_instance == null){
+                _instance = FindFirstObjectByType<T>();
+                if (_instance == null){
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake(){
+        if (_instance == null){
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoad;
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+
+    protected abstract void OnSceneLoad(Scene scene, LoadSceneMode mode);
+
+}
