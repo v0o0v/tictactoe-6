7994c42802df8035c9fc1176d45c0345d36c0d2e
김민석 <v0o0v@daum.net>
2026-02-27 11:31:34 +0900

    add CameraController to manage camera orthographic size based on aspect ratio

---
+++ Assets/Scripts/Common/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Tictactoe {
+
+    [RequireComponent(typeof(Camera))]
+    public class CameraController : MonoBehaviour {
+
+        [SerializeField] private float widthUnit = 6f;
+        Camera _camera;
+
+        private void Start(){
+            _camera = GetComponent<Camera>();
+            _camera.orthographicSize = widthUnit / _camera.aspect / 2f;
+        }
+
+    }
+
+}
